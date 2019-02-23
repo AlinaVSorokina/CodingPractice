d3fd8cdecc6720a12d07771af58bd2042f52ec0b
alina.sorokina <alina_sorokina@epam.com>
2019-02-23 20:23:12 +0300

    19 20

---
+++ src/Util/ListNode.java
@@ -0,0 +1,10 @@
+package Util;
+
+/**
+ * Created by alina on 23.02.19.
+ */
+public class ListNode {
+    public int data;
+    public ListNode next;
+    public ListNode previous;
+}
--- src/preparation/Week3.java
+++ src/preparation/Week3.java
@@ -1,6 +1,8 @@
 package preparation;
 
+import Util.ListNode;
 import Util.LogStructure;
+import org.w3c.dom.NodeList;
 
 import java.util.Deque;
 import java.util.LinkedList;
@@ -9,7 +11,7 @@ import java.util.TreeMap;
 public class Week3 {
 
     public static void main(String[] args) {
-        day18run();
+        day19run();
     }
 
     //------------Day 15
@@ -28,7 +30,7 @@ public class Week3 {
     }
     //------------Day 17
 
-    //------------Day 18
+    //------------Day 18 OK
     public static void day18run() {
         int[] a = {10, 5, 2, 7, 8, 7};
         int k = 3;
@@ -66,39 +68,93 @@ public class Week3 {
         }
     }
 
-    //------------Day 19
+    //------------Day 19 OK
 
-    public static void day9run() {
+    public static void day19run() {
+        int[][] houses = new int[3][3];
+        houses[0][0] = 1;
+        houses[0][1] = 2;
+        houses[0][2] = 3;
+
+        houses[1][0] = 5;
+        houses[1][1] = 4;
+        houses[1][2] = 7;
+
+        houses[2][0] = 3;
+        houses[2][1] = 3;
+        houses[2][2] = 2;
+
+        System.out.print(day19(houses, 3, 3));
 
     }
 
     public static int day19(int[][] houses, int n, int k) {
-        int sum = 0;
-        int ex = -1;
-        for (int i = 0; i < n; i ++) {
-            int[] colors = houses[i];
-            int minIndex = getMin(colors, ex);
-            sum = sum + colors[minIndex];
-            ex = minIndex;
+        int lowest = 0;
+        int secondLowest = 0;
+        int lowestInd = -1;
+
+        for (int i = 0; i < n; i++) {
+            int newLowest = Integer.MAX_VALUE;
+            int newSecondLowest = Integer.MAX_VALUE;
+            int newLowestInd = -1;
+            for (int j = 0; j < k; j++) {
+                int prevLowest = j == lowestInd ? secondLowest : lowest;
+                int cost = prevLowest + houses[i][j];
+                if (cost < newLowest) {
+                    newSecondLowest = newLowest;
+                    newLowest = cost;
+                    newLowestInd = j;
+                } else if (cost < newSecondLowest) {
+                    newSecondLowest = cost;
+                }
+            }
+            lowest = newLowest;
+            lowestInd = newLowestInd;
+            secondLowest = newSecondLowest;
         }
-        return sum;
+        return lowest;
+
     }
 
-    private static int getMin(int[] colors, int ex) {
-        int min = Integer.MAX_VALUE;
-        int minIndex = 0;
-        for (int i = 0; i < colors.length; i++) {
-            if (i != ex && colors[i] < min) {
-                min= colors[i];
-                minIndex = i;
+    //------------Day 20 OK
+
+    public static void day20run() {
+
+    }
+
+    public static ListNode getCommonPoint(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) return null;
+        int firstLength = 1;
+        ListNode head1 = list1;
+        while (head1.next != null) {
+            firstLength++;
+            head1 = head1.next;
+        }
+        int secondLength = 1;
+        ListNode head2 = list2;
+        while (head2.next != null) {
+            secondLength++;
+            head2 = head2.next;
+        }
+        head1 = list1;
+        head2 = list2;
+        if (secondLength > firstLength) {
+            int diff = secondLength - firstLength;
+            for (int i = diff; i > 0; i--) {
+                head2 = head2.next;
             }
         }
-        return minIndex;
+        if (secondLength < firstLength) {
+            int diff = firstLength - secondLength;
+            for (int i = diff; i > 0; i--) {
+                head1 = head1.next;
+            }
+        }
+        while (head1 != null) {
+            if (head1.next == head2.next) return head1.next;
+        }
+        return null;
     }
-
-
-
-    //------------Day 20
 
     //------------Day 21
 
