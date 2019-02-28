c3f34533b48dee590a5ec0db584aef045a644df2
alina.sorokina <alina_sorokina@epam.com>
2019-02-28 21:58:16 +0300

    25 26

--- src/preparation/week4/Day24.java
+++ src/preparation/week4/Day24.java
@@ -1,11 +1,16 @@
 package preparation.week4;
 
 /**
- * Created by alina on 27.02.19.
+ * Design a binary tree node class with the following methods:
+
+    is_locked, which returns whether the node is locked
+    lock, which attempts to lock the node. If it cannot be locked, then it should return false. Otherwise, it should lock it and return true.
+    unlock, which unlocks the node. If it cannot be unlocked, then it should return false. Otherwise, it should unlock it and return true.
+    You may augment the node to add parent pointers or any other property you would like. You may assume the class is used in a single-threaded program, so there is no need for actual locks or mutexes. Each method should run in O(h), where h is the height of the tree.
  */
 public class Day24 {
     public static void main(String[] args) {
-
+        //build tree from locking nodes
     }
 
 
---
+++ src/preparation/week4/Day26.java
@@ -0,0 +1,42 @@
+package preparation.week4;
+
+import Util.ListNode;
+
+/**
+ * Given a singly linked list and an integer k, remove the kth last element from the list.
+ * k is guaranteed to be smaller than the length of the list.
+ * The list is very long, so making more than one pass is prohibitively expensive.
+ * Do this in constant space and in one pass.
+ */
+public class Day26 {
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(3);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
+        ListNode a6 = new ListNode(6);
+        ListNode a7 = new ListNode(7);
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
+        a5.next = a6;
+        a6.next = a7;
+        ListNode res = getKth(a1, 5);
+        System.out.print(res.data);
+    }
+
+    public static ListNode getKth(ListNode head, int k) {
+        ListNode pointer = head;
+        for (int i = 0; i < k; i++) {
+            pointer = pointer.next;
+        }
+        ListNode second = head;
+        while (pointer != null) {
+            pointer = pointer.next;
+            second = second.next;
+        }
+        return second;
+    }
+}
