207841495ee290c45deab272f1fcac639419853e
Alina Sorokina <alina_sorokina@epam.com>
2019-05-05 09:45:25 +0300

    tree node

--- src/Util/TreeNode.java
+++ src/Util/TreeNode.java
@@ -4,4 +4,16 @@ public class TreeNode {
     public int data;
     public TreeNode left;
     public TreeNode right;
+
+    public TreeNode(){}
+
+    public TreeNode(int d) {
+        this.data = d;
+    }
+
+    public TreeNode(int d, TreeNode l, TreeNode r) {
+        this.data = d;
+        this.left = l;
+        this.right = r;
+    }
 }
